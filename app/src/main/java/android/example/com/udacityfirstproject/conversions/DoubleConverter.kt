aa894e2aaea5b3c371f76038bd02c02e34709b93
Mina Wadee <mina.wadae@innovitics.com>
2022-12-16 14:38:26 +0200

    Finish Shoe Details Implementation

---
+++ app/src/main/java/android/example/com/udacityfirstproject/conversions/DoubleConverter.kt
@@ -0,0 +1,21 @@
+package android.example.com.udacityfirstproject.conversions
+
+import androidx.databinding.InverseMethod
+
+object DoubleConverter {
+
+    @InverseMethod("stringToDouble")
+    @JvmStatic
+    fun doubleToString(value: Double?): String {
+        return value?.toString() ?: ""
+    }
+
+    @JvmStatic
+    fun stringToDouble(value: String): Double? {
+        return if (value.isNotEmpty()) {
+            value.toDouble()
+        } else {
+            0.0
+        }
+    }
+}
